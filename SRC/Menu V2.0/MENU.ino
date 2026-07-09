6525494140d566877b12dcc39a98593bcced4d62
Joshua20253AU222 <20253au222@utez.edu.mx>
2026-07-09 11:29:20 -0600

    Avance Semana 10
    + Imágenes (block de prueba, sistema intregador, material, molde de prueba).
    + Simulación en proteus (modulo relevador).
    + Versión 2.0 del sistema integrador.

---
+++ SRC/Menu V2.0/MENU.ino
@@ -0,0 +1,275 @@
+//#include <Adafruit_LiquidCrystal.h>
+//#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+//Adafruit_LiquidCrystal lcd_1(0);
+LiquidCrystal_I2C lcd_1(0x27,20,4);
+
+int arriba = 0;
+int abajo = 0;
+int enter = 0;
+int x = 0;
+bool error = false;
+const int numOp= 2;
+
+
+int velocidad = 800;
+const int stepPin = 0;
+const int dirPin  = 1;
+
+
+int tiempo[] =     {10000,4000,5000,6000};
+
+String menuSel[] = {"->Triturar","->Secuencia","",""};
+String menuDes[] = {"Secuencia","","",""};
+int pines[] =      {2,3,1};
+
+int cursor = 0;
+
+void motor(){
+  lcd_1.clear();
+  lcd_1.print("moviendo motor");
+               
+
+
+  digitalWrite(6, HIGH); //led indicativo  
+  digitalWrite(dirPin, HIGH); //configura pin para girar a sentido horario
+  
+  
+  for (int x = 0; x < 4; x++){
+
+    lcd_1.clear();
+    lcd_1.setCursor(0,0);
+    lcd_1.print("moviendo motor");
+    lcd_1.setCursor(0,1);
+    lcd_1.print ("Horario...");
+    for (int i = 0; i < 400; i++) {  
+      digitalWrite(stepPin, HIGH); //enciende pin de paso
+      delayMicroseconds(velocidad);        
+      digitalWrite(stepPin, LOW);
+      delayMicroseconds(velocidad);
+    
+    
+    }
+    
+    delay(1000);  
+
+    digitalWrite(dirPin, LOW);   //configura pin para girar a sentido anti-horario
+    
+    lcd_1.clear();
+    lcd_1.setCursor(0,0);
+    lcd_1.print("moviendo motor");
+    lcd_1.setCursor(0,1);
+    lcd_1.print ("Anti Horario...");
+
+    for (int i = 0; i < 100; i++) {
+      digitalWrite(stepPin, HIGH);
+      delayMicroseconds(velocidad);
+      digitalWrite(stepPin, LOW);
+      delayMicroseconds(velocidad);
+    }
+
+    delay(1000);  
+
+    digitalWrite(dirPin, HIGH); //configura pin para girar a sentido horario
+
+    lcd_1.clear();
+    lcd_1.setCursor(0,0);
+    lcd_1.print("moviendo motor");
+    lcd_1.setCursor(0,1);
+    lcd_1.print ("Horario...");
+    for (int i = 0; i < 50; i++) {  
+      digitalWrite(stepPin, HIGH); //enciende pin de paso
+      delayMicroseconds(velocidad);        
+      digitalWrite(stepPin, LOW);
+      delayMicroseconds(velocidad);
+    }
+
+    delay (2500);
+  }
+
+    digitalWrite(6, LOW);   
+  
+  delay(1000); 
+
+        lcd_1.clear();
+      lcd_1.setCursor(0,0);
+      lcd_1.print(menuSel[cursor]);
+      lcd_1.setCursor(0,1);
+      lcd_1.print(menuDes[cursor]);
+}
+
+void setup()
+{
+  
+  pinMode(13, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+  
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  pinMode(stepPin, OUTPUT);
+  
+  
+  //lcd_1.begin(16,2);
+  lcd_1.init();
+  lcd_1.backlight();
+
+   lcd_1.print("Encendido...");
+  lcd_1.setCursor(0,1);
+  lcd_1.print("V2.0");
+  lcd_1.setCursor(0,0);
+  delay(1500);
+  
+  lcd_1.clear();
+  
+  lcd_1.print( menuSel[0]);
+  lcd_1.setCursor(0,1);
+  lcd_1.print( menuDes[0]);
+  
+}
+
+void loop()
+{
+  arriba = digitalRead(13);
+  abajo = digitalRead(12);
+  enter = digitalRead(11);
+  
+  
+  
+  if (arriba == LOW) {
+    digitalWrite(10, HIGH);
+    //lcd_1.print("ARRIBA"); //POR SI ACASO
+    
+    if (cursor > 0){
+    	  cursor -=1;
+      lcd_1.clear();
+      lcd_1.setCursor(0,0);
+      lcd_1.print(menuSel[cursor]);
+      lcd_1.setCursor(0,1);
+      lcd_1.print(menuDes[cursor]);
+      delay(1000);
+    }else{
+      error = true;
+    }
+    
+    
+    
+  } 
+  else if (abajo == LOW) {
+    digitalWrite(9, HIGH);
+    //lcd_1.print("ABAJO"); //POR SI ACASO
+
+        if (cursor < numOp-1){
+    	  cursor +=1;
+          lcd_1.clear();
+          lcd_1.setCursor(0,0);
+          lcd_1.print(menuSel[cursor]);
+          lcd_1.setCursor(0,1);
+          lcd_1.print(menuDes[cursor]);
+          delay(1000); 
+    }else{
+          error = true;
+    }
+
+  } 
+  else if (enter == LOW) {
+    digitalWrite(8, HIGH);
+    //lcd_1.print("ENTER"); //POR SI ACASO
+    lcd_1.clear();
+      
+    for(x=1; x<=2; x++){
+        lcd_1.clear();
+      	lcd_1.print(menuSel[cursor]);
+        
+        delay(400);
+        
+    }
+
+    if(cursor==1){
+      motor();
+    }else if (cursor==0){
+int time=0;
+      while(digitalRead(11)==HIGH) {
+        digitalWrite(pines[cursor],HIGH);
+        
+        lcd_1.clear();
+        lcd_1.print(menuSel[cursor]);
+        lcd_1.print (" (");
+          lcd_1.print (time);
+          lcd_1.print (")");
+        delay(1000);
+
+        time++;
+
+      }
+
+      lcd_1.clear();
+      lcd_1.print(menuSel[cursor]);
+      lcd_1.setCursor(0,1);
+      lcd_1.print("Saliendo...");
+
+      delay(2000);
+      digitalWrite(pines[cursor],LOW);
+
+      lcd_1.clear();
+          lcd_1.setCursor(0,0);
+          lcd_1.print(menuSel[cursor]);
+          lcd_1.setCursor(0,1);
+          lcd_1.print(menuDes[cursor]);
+    
+    }else{
+        delay(2000);
+      digitalWrite(pines[cursor],HIGH);
+      for(x=(tiempo[cursor]/1000); x>=0; x--){
+          lcd_1.clear();
+          lcd_1.print(menuSel[cursor]);
+          lcd_1.print (" (");
+          lcd_1.print (x);
+          lcd_1.print (")");
+        digitalWrite(6,HIGH);
+          delay(200);
+          digitalWrite(6,LOW);
+          delay(800);
+
+      }
+      
+      digitalWrite(pines[cursor],LOW);
+      lcd_1.clear();
+      lcd_1.setCursor(0,0);
+      lcd_1.print(menuSel[cursor]);
+      lcd_1.setCursor(0,1);
+      lcd_1.print(menuDes[cursor]);
+      delay(1000);
+    }
+    
+
+
+  }
+  
+  
+  if(error == true){
+    error = false;
+    for(x=1; x<=3; x++){
+        lcd_1.clear();
+      	lcd_1.print("FIN DE LISTA");
+        digitalWrite(7,HIGH);
+        delay(200);
+      	digitalWrite(7,LOW);
+        
+    }
+   	lcd_1.clear();
+    lcd_1.setCursor(0,0);
+    lcd_1.print( menuSel[cursor]);
+    lcd_1.setCursor(0,1);
+    lcd_1.print( menuDes[cursor]);
+    
+  }
+
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(8, LOW);
+}
+
